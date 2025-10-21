895488279fa825ec1cf93bdfa134039d728d1492
Hasanuddin <hasanuddin.hasanuddin@prudentialsyariah.co.id>
2025-10-21 15:45:26 +0700

    Update portfolio

---
+++ assets/js/portfolio-data.js
@@ -0,0 +1,177 @@
+// Centralized portfolio data used by index and portfolio-details.html
+window.PORTFOLIO_DATA = [
+  {
+    id: 1,
+    title: 'Web Agen Bank Mandiri',
+    category: 'Web',
+    filters: 'filter-web filter-core',
+    images: [
+      'assets/img/portfolio/Mandiri-Web.png',
+      'assets/img/portfolio/Mandiri-Web2.png',
+      'assets/img/portfolio/Mandiri-Web3.png'
+    ],
+    tech: 'PHP, CodeIgniter, Bootstrap, Javascript, Mule, Java, Cyclos',
+    date: 'December, 2015',
+    contribution: 'Fullstack Developer',
+    description: 'A web that can make it easier for agents to make Mandiri e-cash transactions'
+  },
+  {
+    id: 2,
+    title: 'Ecash panin One to Many transfer',
+    category: 'Web',
+    filters: 'filter-web filter-core',
+    images: [
+      'assets/img/portfolio/Panin-1toMany.png',
+      'assets/img/portfolio/Panin-1ToMany2.png'
+    ],
+    tech: 'PHP, Laravel, Bootstrap, Javascript, Mule, Java, Cyclos, Batch Processing',
+    date: 'February 2016',
+    contribution: 'Fullstack Developer, Devops',
+    description: 'The Panin e-cash for agents has a feature called "one-to-many transfer" to transfer funds to registered Panin e-cash accounts in bulk.'
+  },
+  {
+    id: 3,
+    title: 'OCBC NISP USSD',
+    category: 'Core',
+    filters: 'filter-core',
+    images: [
+      'assets/img/portfolio/OCBC-USSD.png',
+      'assets/img/portfolio/OCBC-USSD2.png',
+      'assets/img/portfolio/OCBC-USSD3.png',
+      'assets/img/portfolio/OCBC-USSD4.png',
+      'assets/img/portfolio/OCBC-USSD5.png'
+    ],
+    tech: 'Mule, Java, Cyclos, Redis',
+    date: 'January 2017',
+    contribution: 'Fullstack Developer',
+    description: 'Uang elektronik yang memanfaatkan teknologi USSD di telepon genggam, memungkinkan pengguna melakukan transaksi tanpa smartphone.'
+  },
+  {
+    id: 4,
+    title: 'Perbarindo Ecash',
+    category: 'Mobile',
+    filters: 'filter-app',
+    images: [
+      'assets/img/portfolio/BPR-App.png',
+      'assets/img/portfolio/BPR-App2.png',
+      'assets/img/portfolio/BPR-App3.png'
+    ],
+    tech: 'Android, Mule, Java, Cyclos',
+    date: 'October 2017',
+    contribution: 'Fullstack Developer',
+    description: 'Uang Elektronik dalam bentuk aplikasi mobile di Android.'
+  },
+  {
+    id: 5,
+    title: 'Web agen Panin',
+    category: 'Web',
+    filters: 'filter-web filter-core',
+    images: [
+      'assets/img/portfolio/Panin-Web.png',
+      'assets/img/portfolio/Panin-Web2.png'
+    ],
+    tech: 'PHP, Laravel, Bootstrap, Javascript, Mule, Java, Cyclos',
+    date: 'March 2018',
+    contribution: 'Fullstack Developer',
+    description: 'A web that can make it easier for agents to make Panin Bank e-cash transactions'
+  },
+  {
+    id: 6,
+    title: 'SQS OFFline',
+    category: 'App',
+    filters: 'filter-core filter-web',
+    images: [
+      'assets/img/portfolio/SQS-Offline.png'
+    ],
+    tech: 'Java, JSP, Hibernate, SQL',
+    date: 'October 2018',
+    contribution: 'Fullstack Developer',
+    description: 'An application to make Insurance Illustration at Prudential'
+  },
+  {
+    id: 7,
+    title: 'Prufast Mobile',
+    category: 'Mobile',
+    filters: 'filter-app',
+    images: [
+      'assets/img/portfolio/Prufast.png',
+      'assets/img/portfolio/Prufast2.png',
+      'assets/img/portfolio/Prufast3.png'
+    ],
+    tech: 'React Native, Node JS, Sqlite',
+    date: 'October 2018',
+    contribution: 'Mobile Apps Developer',
+    description: 'An application for Insurance Illustration at Prudential (Mobile Version)'
+  },
+  {
+    id: 8,
+    title: 'PruhubSQS Configurator',
+    category: 'Web',
+    filters: 'filter-web filter-core',
+    images: [
+      'assets/img/portfolio/Pruhub.png',
+      'assets/img/portfolio/Pruhub2.png'
+    ],
+    tech: 'AngularJS, Java, Grails, PostgresSql, cron Job',
+    date: 'October 2019',
+    contribution: 'Back End Developer',
+    description: 'Product Configuration for Mobile Apps SQLite'
+  },
+  {
+    id: 9,
+    title: 'Prufast Cekatan Non Face to Face',
+    category: 'Web',
+    filters: 'filter-web',
+    images: [
+      'assets/img/portfolio/Microsite-NF2F.png'
+    ],
+    tech: 'React Js, Typescript',
+    date: 'April 2020',
+    contribution: 'Front End Developer',
+    description: 'Web microservice for customer signature when face-to-face is not possible'
+  }
+  ,
+  {
+    id: 10,
+    title: 'BackOffice',
+    category: 'Web',
+    filters: 'filter-web filter-core',
+    images: [
+      'assets/img/portfolio/BackOffice.png',
+      'assets/img/portfolio/BackOffice2.png'
+    ],
+    tech: 'Java, Quarkus, React',
+    date: 'May 2024',
+    contribution: 'Back End Developer',
+    description: 'Back office web application for administrative workflows.'
+  },
+  {
+    id: 11,
+    title: 'Polisku',
+    category: 'Web',
+    filters: 'filter-web',
+    images: [
+      'assets/img/portfolio/Polisku.png',
+      'assets/img/portfolio/Polisku2.png'
+    ],
+    tech: 'Vertx, React',
+    date: 'July 2025',
+    contribution: 'Fullstack Developer',
+    description: 'Policy management web application.'
+  },
+  {
+    id: 12,
+    title: 'Pruservices',
+    category: 'Web',
+    filters: 'filter-web filter-core',
+    images: [
+      'assets/img/portfolio/Pruservices.png',
+      'assets/img/portfolio/Pruservices2.png',
+      'assets/img/portfolio/Pruservices3.png'
+    ],
+    tech: 'React, Java, Quarkus, Microservices',
+    date: 'August 2022',
+    contribution: 'Back End Developer',
+    description: 'Microservice-based backend for platform services.'
+  }
+];
